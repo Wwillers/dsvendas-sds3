e6fa38ab3d632edd1513a4a4552863953a72f16c
William <w.willers@outlook.com>
2021-05-04 22:01:39 -0400

    Iniciando o projeto

---
+++ backend/src/test/java/com/sds3/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds3.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
