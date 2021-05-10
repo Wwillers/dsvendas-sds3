68a0a1a44dc6420160fbc092265ccf369d111d97
William <w.willers@outlook.com>
2021-05-09 21:20:19 -0400

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,27 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/request';
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+};
 
 function DonutChart() {
+  let chartData: ChartData = {
+    labels: [],
+    series: [],
+  };
+
+  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+    const data = response.data as SaleSum[];
+    const myLabels = data.map((label) => label.sellerName);
+    const mySeries = data.map((series) => series.sum);
+
+    chartData = { labels: myLabels, series: mySeries };
+  });
+
   const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
