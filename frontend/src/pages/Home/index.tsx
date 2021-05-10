c9e0523b408b92c2921c2ad28d85ee553e6cae19
William <w.willers@outlook.com>
2021-05-09 20:59:08 -0400

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,32 @@
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Página Home</h1>
-    </div>
-  )
-}
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <section className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot. <br />
+            <Link className="btn btn-primary btn-large" to="/dashboard">
+              Acessar dashboard
+            </Link>
+          </p>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
